9054fe94204d7ccc007bfea7a57bc94c111fc118
Jon Huang <jon@orderly.network>
2022-09-03 07:13:23 +0800

    solution2

--- token-contract/src/lib.rs
+++ token-contract/src/lib.rs
@@ -1,5 +1,4 @@
 use near_contract_standards;
-use near_contract_standards::fungible_token::events::FtTransfer;
 use near_contract_standards::fungible_token::metadata::{FungibleTokenMetadata, FT_METADATA_SPEC};
 use near_contract_standards::fungible_token::FungibleToken;
 
@@ -13,12 +12,11 @@ use near_sdk::{
 /// a fungible token template
 /// we can deploy to multiple accounts and init with different parameters to create different tokens
 /// there is a amm_id attribute which reffers to the AMM Contract
-/// we use this attribute to authorize and verify transfer_from method 
+/// we use this attribute to authorize and verify transfer_from method
 /// this is not a proper way when using in production
 #[near_bindgen]
 #[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
 pub struct Contract {
-    amm_id: Option<AccountId>,
     token: FungibleToken,
     meta: FungibleTokenMetadata,
 }
@@ -40,7 +38,6 @@ impl Contract {
     ) -> Self {
         require!(!env::state_exists(), "The contract has initialized!");
         let mut this = Self {
-            amm_id: None,
             token: FungibleToken::new(StorageKey::T),
             meta: FungibleTokenMetadata {
                 spec: FT_METADATA_SPEC.to_string(),
@@ -54,46 +51,13 @@ impl Contract {
         };
 
         this.token.internal_register_account(&owner_id);
-        this.token.internal_deposit(&owner_id, total_supply * 10_u128.pow(decimals as u32));
+        this.token
+            .internal_deposit(&owner_id, total_supply * 10_u128.pow(decimals as u32));
         this
     }
 
     pub fn get_info(self) -> (String, u8) {
         (self.meta.symbol, self.meta.decimals)
-    }
-
-    #[payable]
-    pub fn register_amm(&mut self, sender_id: AccountId, amount: Balance) {
-        require!(self.amm_id.is_none(), "amm has been registered");
-        let receiver_id = env::predecessor_account_id();
-        self.amm_id = Some(receiver_id.clone());
-        self.token.internal_register_account(&receiver_id);
-        self.transfer_from(sender_id, receiver_id, amount)
-    }
-
-    #[payable]
-    pub fn transfer_from(&mut self, sender_id: AccountId, receiver_id: AccountId, amount: Balance) {
-        // allow amm contracts to transfer tokens on their behalf
-        // TODO need account to approve the contracts.
-        require!(
-            Some(env::predecessor_account_id()) == self.amm_id,
-            "caller must the amm contract"
-        );
-
-        require!(sender_id != receiver_id, "Sender and receiver should be different");
-        require!(amount > 0, "The amount should be a positive number");
-        self.token.internal_withdraw(&sender_id, amount);
-        if !self.token.accounts.contains_key(&receiver_id) {
-            self.token.internal_register_account(&receiver_id);
-        }
-        self.token.internal_deposit(&receiver_id, amount);
-        FtTransfer {
-            old_owner_id: &sender_id,
-            new_owner_id: &receiver_id,
-            amount: &U128(amount),
-            memo: None,
-        }
-        .emit();
     }
 
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
@@ -144,5 +108,4 @@ mod tests {
         assert_eq!(contract.ft_balance_of(accounts(1)).0, total_supply);
         assert_eq!(contract.get_info(), (name.into(), decimals));
     }
-    
 }
